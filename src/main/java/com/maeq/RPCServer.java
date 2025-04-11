f45afd0f8202c1dffe141f28586bb26e269b5aff
meqbyte <meq1986@163.com>
2025-04-11 22:02:42 +0800

    使用最简单的input stream

--- src/main/java/com/maeq/RPCServer.java
+++ src/main/java/com/maeq/RPCServer.java
@@ -1,6 +1,7 @@
 package com.maeq;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,6 +13,16 @@ public class RPCServer {
             ServerSocket serverSocket = new ServerSocket(8888);
             Socket socket = serverSocket.accept();
 
+            InputStream in = socket.getInputStream();
+
+            int data;
+            while ((data = in.read()) != -1) {
+                System.out.println("data = " + data);
+            }
+
+            in.close();
+            socket.close();
+            serverSocket.close();
 
 
         } catch (IOException e) {
