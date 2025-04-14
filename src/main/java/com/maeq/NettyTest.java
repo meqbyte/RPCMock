4e8c7e311027bc586fecb9ce132f6ae36cc42f93
meqbyte <meq1986@163.com>
2025-04-14 21:29:21 +0800

    update

---
+++ src/main/java/com/maeq/NettyTest.java
@@ -0,0 +1,26 @@
+package com.maeq;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyTest {
+
+    public static void main(String[] args) {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<>() {
+
+                    });
+        }
+
+    }
+
+}
